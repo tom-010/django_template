57752c4483de9a69be388c3ce2eba4037bea31fd
Thomas Deniffel <tdeniffel@gmail.com>
2021-12-15 13:37:21 +0100

    init_template: install virtualenv

--- init_template.py
+++ init_template.py
@@ -1,1 +1,8 @@
-print('hello')
+import subprocess
+
+def run(command):
+    print(subprocess.check_output(command.split(), stderr=subprocess.STDOUT).decode())
+
+run('python3 -m pip install virtualenv')
+run('python3 -m virtualenv env')
+run('pip3 install -r requirements.txt')
